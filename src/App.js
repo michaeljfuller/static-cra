8ce567e110a0ef6f0817a3d288218e72c6c26a35
Adam Tamics <57900951+adamtamics@users.noreply.github.com>
2023-03-24 15:19:05 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome from Kinsta Static Site Hosting
+          Welcome from Kinsta Static Site Hosting!
         </p>
       </header>
     </div>
